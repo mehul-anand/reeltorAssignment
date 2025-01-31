738385911245b55c49b0a561bb970a727d020576
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 07:54:17 +0530

    table component improvement

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -31,7 +31,7 @@ function Table({ type, timeFrame, categoryArray, dataArray }) {
           </thead>
           <tbody>
             {indexArray.map((num) => (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <tr key={num} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                   <th
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
