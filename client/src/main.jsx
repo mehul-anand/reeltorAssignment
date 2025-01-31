a88a4d7d9b136991af150fa32669b9ea946e85eb
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 05:41:00 +0530

    routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Pages from "./pages"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Pages from "./pages";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import Sales from "./components/Sales.jsx";
 
-const {Home,Layout,Dashboard,Listings} = Pages
+const { Home, Layout, Dashboard, Listings } = Pages;
 
-const appRouter = createBrowserRouter(createRoutesFromElements(
-  <>
-  <Route path='' element={<Home/>}/>
-  <Route path='/' element={<Layout/>}>
-    <Route path='dashboard' element={<Dashboard/>}/>
-    <Route path='listings' element={<Listings/>}/>
-  </Route>
-  </>
-))
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="sales" element={<Sales />} />
+        </Route>
+        <Route path="listings" element={<Listings />} />
+      </Route>
+    </>
+  )
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={appRouter} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
