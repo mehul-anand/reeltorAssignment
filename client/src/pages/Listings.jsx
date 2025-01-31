a7e09ebbf7538ad5cc8d989aaecef739124e238d
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 15:12:46 +0530

    searching changes

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -10,8 +10,9 @@ function Listings() {
 
   const filteredList = listData.filter((property) => {
     const matchesCity = selectedCity ? property.city === selectedCity : true;
-    const matchesSearch =
-      property.title.toLowerCase().includes(searchTerm.toLowerCase())
+    const matchesSearch = property.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
     return matchesCity && matchesSearch;
   });
 
@@ -42,10 +43,10 @@ function Listings() {
       <div className="p-4 sm:ml-64 bg-gray-50 dark:bg-gray-800">
         <div className="p-4 mt-14 grid grid-cols-6 grid-rows-8 gap-4 sm:grid-cols-12 sm:grid-rows-1 bg-gray-50 dark:bg-gray-700">
           <div className="sm:row-span-1 sm:col-span-4">
-            <form class="mx-auto">
+            <form className="mx-auto">
               <label
-                for="city"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="city"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Chose a city
               </label>
@@ -53,7 +54,7 @@ function Listings() {
                 id="city"
                 value={selectedCity}
                 onChange={handleCityChange}
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option value="">All Cities</option>
                 <option value="Delhi">Delhi</option>
@@ -64,17 +65,17 @@ function Listings() {
             </form>
           </div>
           <div className="sm:row-span-1 sm:col-span-8">
-            <form class="mx-auto">
+            <form className="mx-auto">
               <label
-                for="search"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="search"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Search
               </label>
-              <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                   <svg
-                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -82,9 +83,9 @@ function Listings() {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                     />
                   </svg>
@@ -92,14 +93,14 @@ function Listings() {
                 <input
                   type="search"
                   id="default-search"
-                  class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search properties..."
                   value={searchTerm}
                   onChange={handleSearchChange}
                 />
                 <button
                   type="submit"
-                  class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Search
                 </button>
@@ -110,24 +111,27 @@ function Listings() {
           {filteredList.length > 0 ? (
             <>
               {filteredList.slice(page * 9 - 9, page * 9).map((propInfo) => (
-                <>
-                  <div className="sm:col-span-4 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-4 col-span-6">
-                    <Card propertyInfo={propInfo} />
-                  </div>
-                </>
+                <div
+                  key={propInfo.id}
+                  className="sm:col-span-4 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-4 col-span-6"
+                >
+                  <Card propertyInfo={propInfo} />
+                </div>
               ))}
             </>
-          ):(<div className="sm:col-span-12 sm:row-span-4">
-          <Error/>
-          </div>)}
-          <div class="flex sm:row-span-2 sm:col-span-12">
+          ) : (
+            <div className="sm:col-span-12 sm:row-span-4">
+              <Error />
+            </div>
+          )}
+          <div className="flex sm:row-span-2 sm:col-span-12">
             <a
               onClick={prevFunc}
               href="#"
-              class="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               <svg
-                class="w-3.5 h-3.5 me-2 rtl:rotate-180"
+                className="w-3.5 h-3.5 me-2 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -135,9 +139,9 @@ function Listings() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M13 5H1m0 0 4 4M1 5l4-4"
                 />
               </svg>
@@ -146,18 +150,18 @@ function Listings() {
             <a
               href="#"
               aria-current="page"
-              class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              className="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
             >
               {page}
             </a>
             <a
               onClick={nextFunc}
               href="#"
-              class="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Next
               <svg
-                class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -165,9 +169,9 @@ function Listings() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
