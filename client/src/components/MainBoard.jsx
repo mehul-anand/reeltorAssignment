6a3210f65c4e08838f982ee9a3eb04c26b8f77bd
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 09:08:18 +0530

    main board component

--- client/src/components/MainBoard.jsx
+++ client/src/components/MainBoard.jsx
@@ -7,11 +7,14 @@ import { userData } from "../data/dummyData";
 import { dayTimes } from "../data/dummyData";
 import { dailySales } from "../data/dummyData";
 import { dailyViews } from "../data/dummyData";
+import ComparisonChart from "./ComparisonChart";
+import PieChart from "./PieChart";
 
 function MainBoard() {
   return (
     <div className="p-4 mt-14 grid grid-cols-6 grid-rows-8 gap-4 sm:grid-cols-12 sm:grid-rows-13 bg-gray-50 dark:bg-gray-700">
-      <div className="sm:col-span-12 sm:grid-rows-1 col-span-6">
+      {/* Row 1 */}
+      <div className="sm:col-span-12 sm:row-span-1 col-span-6">
         <p className="text-2xl text-gray-900 dark:text-white sm:pr-5 sm:pt-2">
           Hello {userData.name}!
         </p>
@@ -19,7 +22,8 @@ function MainBoard() {
           Here are your stats
         </p>
       </div>
-      <div className="sm:col-span-6 sm:row-span-3 bg-gray-50 h-28 dark:bg-gray-800 row-span-2 col-span-6 hidden sm:block">
+      {/* Row 2 */}
+      <div className="sm:col-span-6 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-2 col-span-6">
         <ColChart
           firstType={"Sales"}
           firstData={weeklySales}
@@ -29,7 +33,7 @@ function MainBoard() {
           timeFrame={"weekly"}
         />
       </div>
-      <div className="sm:col-span-6 sm:row-span-3 bg-gray-50 h-28 dark:bg-gray-800 row-span-2 col-span-6 hidden sm:block">
+      <div className="sm:col-span-6 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-2 col-span-6">
         <ColChart
           firstType={"Sales"}
           firstData={dailySales}
@@ -38,6 +42,19 @@ function MainBoard() {
           criteriaArray={dayTimes}
           timeFrame={"daily"}
         />
+      </div>
+      {/* Row 3 */}
+      <div className="sm:col-span-8 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-2 col-span-6">
+        <ComparisonChart
+          firstType={"Sales"}
+          firstData={weeklySales}
+          secondType={"Views"}
+          secondData={weeklyViews}
+          criteriaArray={weekDays}
+        />
+      </div>
+      <div className="sm:col-span-4 sm:row-span-4 bg-gray-50 dark:bg-gray-800 row-span-2 col-span-6">
+        <PieChart />
       </div>
     </div>
   );
