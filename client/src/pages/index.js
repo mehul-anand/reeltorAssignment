4801783bfc1bf33186b1e60f6975fe172a188929
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 15:12:56 +0530

    basic exports

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import Layout from "./Layout";
+import Listings from "./Listings";
+
+export default { Home, Dashboard, Layout, Listings };
