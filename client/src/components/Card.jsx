b154fd81c1f7383381f50690800b802fe04f45f6
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2025-01-31 13:46:24 +0530

    card component location

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -14,7 +14,7 @@ function Card({ propertyInfo }) {
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {propertyInfo.address}
+            {propertyInfo.address} , {propertyInfo.city}
           </p>
           <a
             href="#"
